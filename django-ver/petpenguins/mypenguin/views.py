ade8a5a0df59684fc5353a09226ffc22e24c876d
Lucy Mo <lululucyyyyyyy@Lucys-MacBook-Pro-2.local>
2023-02-04 15:01:54 -0500

    users to html

--- django-ver/petpenguins/mypenguin/views.py
+++ django-ver/petpenguins/mypenguin/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render, redirect
 from . import forms
 from datetime import datetime
 from django.contrib.auth import login, authenticate
+from .models import User
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html')
 
 def home(request):
-  return render(request, 'home.html')
+  all_active = User.objects.all().values('username')
+  all_active = list(all_active)
+  all_active_list = [user['username'] for user in all_active]
+  print('here', all_active_list)
+  context = {
+    'users': all_active_list,
+  }
+  return render(request, 'home.html', context)
 
 def register(request):
     if request.method == 'POST':
@@ -16,7 +24,7 @@ def register(request):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.lastActive = datetime.now()
-            print(form.cleaned_data)
+            instance.isActive = True
             form.save()
 
             # messages.success(request, f'Your account has been created. You can log in now!')    
@@ -31,9 +39,7 @@ def login(request):
   if request.method == 'POST':
     username = request.POST['username']
     password = request.POST['password']
-    print(username, password, "HERE")
     user = authenticate(request, username=username, password=password)
-    print(user)
     if user is not None:
         return redirect('home')
     else:
